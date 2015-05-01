541b1e6825f99009bea2e15a58c70795b18ee4b4
Justin Angevaare <jangevaa@uoguelph.ca>
2015-05-01 16:08:21 -0400

    Start writing utilities to improve ease of use

--- src/test.jl
+++ src/test.jl
@@ -4,22 +4,15 @@ Justin Angevaare
 May 2015
 """
 
-# Initial age distribution of adults
-s_db = stock_db(DataFrame(age_2=30000,
-                          age_3=20000,
-                          age_4=15000,
-                          age_5=10000,
-                          age_6=8000))
-
 # Stock assumptions - survivorship and fecundity (proportion sexually mature and mean brood size at age)
 s_a = stock_assumptions([0.35, 0.45, 0.4, 0.35, 0.2],
                         [0.1, 0.5, 0.9, 1, 1],
                         [7500, 15000, 20000, 22500, 25000])
 
 # Agent assumptions - weekly mortality risks and growth (weeks until next stage)
-a_a = agent_assumptions([[0.05 0.1 0.05]
-                         [0.05 0.1 0.05]],
-                         [0.1, 0.2, 0.15],
+a_a = agent_assumptions([[0.002 0.002 0.002]
+                         [0.004 0.004 0.004]],
+                         [0.005, 0.01, 0.01],
                          [19, 52, 104],
                          fill(0.0, (9,9,3)))
 
@@ -32,34 +25,13 @@ e_a = environment_assumptions(reshape(1:9, (3,3)),
                rand(1:2, (3,3)),
                rand(Bool, (3,3)))
 
-# Try create_agent_db
-a_db = create_agent_db(10)
-
-# Try the spawn! function
-spawn!(a_db, s_db, s_a, e_a, 1)
-a_db[1,1]
-
-# Try the kill! function
-kill!(a_db, e_a, a_a, 1, 1)
-a_db[1,1]
-
-# Try the move! function
-move!(a_db, a_a, 1, 1)
-a_db[1,1]
-
-# Try the age_adults! function
-age_adults!(s_db, s_a)
-
-# Try the graduate! function
-graduate!(a_db, s_db, a_a, 1, 1)
-a_db[1,1]
-
-# Try the inject_agents! function
-inject_agents!(a_db, 9, 10000, 1, 1)
-a_db[1,1]
+# Must set initial age distribution of adults
+s_db = stock_db(DataFrame(age_2=30000,
+                          age_3=20000,
+                          age_4=15000,
+                          age_5=10000,
+                          age_6=8000))
 
 # Try the simulate function
-a_db = simulate(2, s_db, s_a, a_a, e_a)
-s_db.population
-
-a_db[1,2]
+a_db = simulate(25, s_db, s_a, a_a, e_a)
+s_d
---
+++ src/utilities.jl
@@ -0,0 +1,5 @@
+"""
+Tools/utilities to improve ease of use of Fish_ABM.jl
+Justin Angevaare
+May 2015
+"""
