d4e59584a060fc70ef3293915edf7e397fa71eed
Justin Angevaare <jangevaa@uoguelph.ca>
2015-05-04 10:15:32 -0400

    equality issue with movement function

--- src/utilities.jl
+++ src/utilities.jl
@@ -11,36 +11,36 @@ function movement_matrix(weights::Array, environment_assumptions::environment_as
   movement = eye(prod(size(environment_assumptions.id)))
   for r in 1:size(environment_assumptions.id)[1]
     for c in 1:size(environment_assumptions.id)[2]
-      if 0 < r-1 < size(environment_assumptions.id)[1]
-        if 0 < c-1 < size(environment_assumptions.id)[2] && environment_assumptions.id[r-1,c-1] != NaN
+      if 0 < r-1 <= size(environment_assumptions.id)[1]
+        if 0 < c-1 <= size(environment_assumptions.id)[2] && environment_assumptions.id[r-1,c-1] != NaN
           movement[environment_assumptions.id[r,c], environment_assumptions.id[r-1,c-1]] = weights[1,1]
         end
-        if 0 < c < size(environment_assumptions.id)[2] && environment_assumptions.id[r-1,c] != NaN
+        if 0 < c <= size(environment_assumptions.id)[2] && environment_assumptions.id[r-1,c] != NaN
           movement[environment_assumptions.id[r,c], environment_assumptions.id[r-1,c]] = weights[1,2]
         end
-        if 0 < c+1 < size(environment_assumptions.id)[2] && environment_assumptions.id[r-1,c+1] != NaN
+        if 0 < c+1 <= size(environment_assumptions.id)[2] && environment_assumptions.id[r-1,c+1] != NaN
           movement[environment_assumptions.id[r,c], environment_assumptions.id[r-1,c+1]] = weights[1,3]
         end
       end
-      if 0 < r < size(environment_assumptions.id)[1]
+      if 0 < r <= size(environment_assumptions.id)[1]
         if 0 < c-1 < size(environment_assumptions.id)[2] && environment_assumptions.id[r,c-1] != NaN
           movement[environment_assumptions.id[r,c], environment_assumptions.id[r,c-1]] = weights[2,1]
         end
-        if 0 < c < size(environment_assumptions.id)[2] && environment_assumptions.id[r,c] != NaN
+        if 0 < c <= size(environment_assumptions.id)[2] && environment_assumptions.id[r,c] != NaN
           movement[environment_assumptions.id[r,c], environment_assumptions.id[r,c]] = weights[2,2]
         end
-        if 0 < c+1 < size(environment_assumptions.id)[2] && environment_assumptions.id[r,c+1] != NaN
+        if 0 < c+1 <= size(environment_assumptions.id)[2] && environment_assumptions.id[r,c+1] != NaN
           movement[environment_assumptions.id[r,c], environment_assumptions.id[r,c+1]] = weights[2,3]
         end
       end
-      if 0 < r+1 < size(environment_assumptions.id)[1]
-        if 0 < c-1 < size(environment_assumptions.id)[2] && environment_assumptions.id[r+1,c-1] != NaN
+      if 0 < r+1 <= size(environment_assumptions.id)[1]
+        if 0 < c-1 <= size(environment_assumptions.id)[2] && environment_assumptions.id[r+1,c-1] != NaN
           movement[environment_assumptions.id[r,c], environment_assumptions.id[r+1,c-1]] = weights[3,1]
         end
-        if 0 < c < size(environment_assumptions.id)[2] && environment_assumptions.id[r+1,c] != NaN
+        if 0 < c <= size(environment_assumptions.id)[2] && environment_assumptions.id[r+1,c] != NaN
           movement[environment_assumptions.id[r,c], environment_assumptions.id[r+1,c]] = weights[3,2]
         end
-        if 0 < c+1 < size(environment_assumptions.id)[2] && environment_assumptions.id[r,c+1] != NaN
+        if 0 < c+1 <= size(environment_assumptions.id)[2] && environment_assumptions.id[r,c+1] != NaN
           movement[environment_assumptions.id[r,c], environment_assumptions.id[r+1,c+1]] = weights[3,3]
         end
       end
