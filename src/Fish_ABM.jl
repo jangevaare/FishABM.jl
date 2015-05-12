55f483d51216cf4302a5fdbff26257949d45701d
Justin Angevaare <jangevaa@uoguelph.ca>
2015-05-12 14:53:02 -0400

    rearrange package slightly

--- src/Fish_ABM.jl
+++ src/Fish_ABM.jl
@@ -2,13 +2,6 @@ module Fish_ABM
 
 using DataFrames, Distributions, Gadfly
 
-include("types.jl")
-include("utilities.jl")
-include("agents.jl")
-include("stock.jl")
-include("agent_stock_interaction.jl")
-include("simulate.jl")
-
 export
   # Types
   agent_assumptions
@@ -17,7 +10,7 @@ export
   stock_db
 
   # Agent functions
-  create_agent_db!
+  create_agent_db
   kill!
   move!
   inject_agents!
@@ -32,5 +25,12 @@ export
 
   # Utilities
   movement_matrix
+
+include("types.jl")
+include("utilities.jl")
+include("agents.jl")
+include("stock.jl")
+include("agent_stock_interaction.jl")
+include("simulate.jl")
 
 end # module
