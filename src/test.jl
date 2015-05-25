6d0c6a9eee2456e8acb9f0c130c318e34b5f33e0
Justin Angevaare <jangevaa@uoguelph.ca>
2015-05-25 14:35:35 -0400

    update test.jl with realistic parameters

--- src/test.jl
+++ src/test.jl
@@ -8,14 +8,14 @@ May 2015
 using DataFrames, Distributions, Fish_ABM
 
 # Stock assumptions
-s_a = stock_assumptions([0.50, 0.50, 0.45, 0.4, 0.4],
+s_a = stock_assumptions([0.50, 0.50, 0.50, 0.50, 0.50, 0.50, 0.50],
                         5,
-                        [7500, 15000, 20000, 22500, 25000],
+                        [2500, 7500, 15000, 20000, 22500, 27500, 32500],
                         500000,
+                        2,
                         0.25,
-                        0.25,
-                        0.25,
-                        [0.00001, 0.00002, 0.000025, 0.000025, 0.000025])
+                        0.5,
+                        [0.00001, 0.00002, 0.000025, 0.000025, 0.000025, 0.000025, 0.000025])
 
 # Randomly generate a simple 3x3 environment_assumptions (id, spawning areas, habitat type and risk1)
 e_a = environment_assumptions(reshape(1:(4*4), (4,4)),
@@ -46,12 +46,16 @@ s_db1 = stock_db(DataFrame(age_2=500000,
                            age_3=50000,
                            age_4=20000,
                            age_5=6000,
-                           age_6=4000),
+                           age_6=4000,
+                           age_7=3000,
+                           age_8=2000),
                  DataFrame(age_2=Int[],
                            age_3=Int[],
                            age_4=Int[],
                            age_5=Int[],
-                           age_6=Int[]))
+                           age_6=Int[],
+                           age_7=Int[]
+                           age_8=Int[]))
 
 # Note: in reduced output mode
 @time a_db1 = simulate(75, fill(0., 75), s_db1, s_a, a_a, e_a, true)
