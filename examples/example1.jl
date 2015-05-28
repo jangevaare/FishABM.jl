c8e04b49c1b93905c44f94e945b45cec62ddf6c3
Justin Angevaare <jangevaa@uoguelph.ca>
2015-05-28 13:16:22 -0400

    update comments in example1.jl

--- examples/example1.jl
+++ examples/example1.jl
@@ -1,13 +1,21 @@
 """
-Demonstrate Fish_ABM functionality
+example1.jl
 Justin Angevaare
 May 2015
 """
 
-# To use package
+# LOAD REQUIRED PACKAGES
 using DataFrames, Distributions, Gadfly, Fish_ABM
 
-# Stock assumptions
+# STOCK ASSUMPTIONS
+# Age specific mortality
+# Age at 50% maturity
+# Age specific fecundity
+# Carrying capacity (total adults)
+# Compensatory strength - fecundity
+# Compensatory strength - age at 50% maturity
+# Compensatory strength - adult natural mortality
+# Age specific catchability
 s_a = stock_assumptions([0.50, 0.50, 0.50, 0.50, 0.50, 0.50, 0.50],
                         5,
                         [2500, 7500, 15000, 20000, 22500, 27500, 32500],
@@ -17,22 +25,30 @@ s_a = stock_assumptions([0.50, 0.50, 0.50, 0.50, 0.50, 0.50, 0.50],
                         0.5,
                         [0.00001, 0.00002, 0.000025, 0.000025, 0.000025, 0.000025, 0.000025])
 
-# Randomly generate a simple 3x3 environment_assumptions (id, spawning areas, habitat type and risk1)
+# LOAD CSV FILES
+# Location ID
+# Spawning areas
+# Habitat types
+# Risk areas
 e_a = environment_assumptions(readdlm(Pkg.dir("Fish_ABM")"/examples/LakeHuron_id.csv", ',', Int),
                               readdlm(Pkg.dir("Fish_ABM")"/examples/LakeHuron_spawning.csv", ',', Bool),
                               readdlm(Pkg.dir("Fish_ABM")"/examples/LakeHuron_habitat.csv", ',', Int),
                               readdlm(Pkg.dir("Fish_ABM")"/examples/LakeHuron_risk.csv", ',', Bool))
 
-spy(e_a.id .== -1)
+# AGENT ASSUMPTIONS
+# Weekly natural mortality rate (by habitat type in the rows, and stage in the columns)
+# Weekly risk mortality (by stage)
+# Stage length (in weeks)
+# Specify empty array for movement
 
-# Agent assumptions - weekly mortality risks and growth (weeks until next stage)
 a_a = agent_assumptions([[0.05 0.05 0.05]
                          [0.05 0.05 0.05]],
                          [0.05, 0.05, 0.05],
                          [19, 52, 104],
                          fill(0.0, (16,16,3)))
 
-# Try the movement_matrix function to specify the movement transition probability matrix
+# MOVEMENT MATRICES
+# Stage specific
 a_a.movement[:,:,1] = movement_matrix([[0 0 0]
                                        [0 1 0]
                                        [0 0 0]], e_a)
@@ -43,7 +59,9 @@ a_a.movement[:,:,3] = movement_matrix([[1 1 2]
                                        [1 3 3]
                                        [1 2 2]], e_a)
 
-# Must set initial age distribution of adults, and create an empty dataframe for fishing mortality
+# STOCK DATABASE
+# Must set initial age distribution of adults
+# Must create an empty dataframe for fishing mortality
 s_db = stock_db(DataFrame(age_2=500000,
                            age_3=50000,
                            age_4=20000,
@@ -59,8 +77,18 @@ s_db = stock_db(DataFrame(age_2=500000,
                            age_7=Int[]
                            age_8=Int[]))
 
-# Note: in reduced output mode
-@time a_db = simulate(75, fill(0., 75), s_db, s_a, a_a, e_a, true)
+# SIMULATE
+# Simulation length (years)
+# Annual fishing effort (vector matching the simulation length)
+# Stock database
+# Stock assumptions
+# Agent assumptions
+# Environment assumptions
+# Reduced output mode (true = reduced output mode, false = full output mode)
+@time a_db = simulate(75, fill(0., 75), s_db, s_a, a_a, e_a, false)
+
+# OUTPUT
+# Adult age distribution through time
 s_db.population
 
 
