816c6e2dd94ec994c9a13e007adccdc6d867a58e
Justin Angevaare <jangevaa@uoguelph.ca>
2015-05-29 15:46:43 -0400

    fix padding function

--- src/utilities.jl
+++ src/utilities.jl
@@ -56,16 +56,17 @@ function PadEnvironmentAssumptions!(EnvironmentAssumptions::EnvironmentAssumptio
   """
   A basic utility function which will pad the EnvironmentAssumptions such that bounds errors do not occur when performing movement
   """
-  a = fill(-1, (size(EnvironmentAssumptions.id, 1)+2, size(EnvironmentAssumptions.id, 1)+2))
+  a = fill(-1, (size(EnvironmentAssumptions.id, 1)+2, size(EnvironmentAssumptions.id, 2)+2))
   a[2:end-1, 2:end-1] = EnvironmentAssumptions.id
   EnvironmentAssumptions.id = a
-  a = fill(false, (size(EnvironmentAssumptions.spawning, 1)+2, size(EnvironmentAssumptions.spawning, 1)+2))
+  a = fill(false, (size(EnvironmentAssumptions.spawning, 1)+2, size(EnvironmentAssumptions.spawning, 2)+2))
   a[2:end-1, 2:end-1] = EnvironmentAssumptions.spawning
   EnvironmentAssumptions.spawning = a
-  a = fill(0, (size(EnvironmentAssumptions.habitat, 1)+2, size(EnvironmentAssumptions.habitat, 1)+2))
+  a = fill(0, (size(EnvironmentAssumptions.habitat, 1)+2, size(EnvironmentAssumptions.habitat, 2)+2))
   a[2:end-1, 2:end-1] = EnvironmentAssumptions.habitat
   EnvironmentAssumptions.habitat = a
-  a = fill(false, (size(EnvironmentAssumptions.risk, 1)+2, size(EnvironmentAssumptions.risk, 1)+2))
+  a = fill(false, (size(EnvironmentAssumptions.risk, 1)+2, size(EnvironmentAssumptions.risk, 2)+2))
   a[2:end-1, 2:end-1] = EnvironmentAssumptions.risk
   EnvironmentAssumptions.risk = a
+  return EnvironmentAssumptions
 end
