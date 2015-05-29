8995efbace8e715e0d1f695fc13f472ddfbd6843
Justin Angevaare <jangevaa@uoguelph.ca>
2015-05-29 17:45:57 -0400

    utilities, types, stock, fishabm...

--- src/stock.jl
+++ src/stock.jl
@@ -22,16 +22,16 @@ function AgeAdults!(StockDB::StockDB, StockAssumptions::StockAssumptions)
   """
   This function will apply transition probabilities to the current adult population. In the future this function may also apply annual removals due to fishing or other causes of mortality.
   """
-  stock_size = fill(0, size(StockAssumptions.natural_mortality))
-  if isnan(StockAssumptions.mortality_compensation)
+  stock_size = fill(0, size(StockAssumptions.naturalmortality))
+  if isnan(StockAssumptions.mortalitycompensation)
     compensation_factor = 1
   else
-    compensation_factor = 2*(cdf(Normal(StockAssumptions.carrying_capacity, StockAssumptions.carrying_capacity/StockAssumptions.mortality_compensation), sum(StockDB.population[end,:][1,])))
+    compensation_factor = 2*(cdf(Normal(StockAssumptions.carryingcapacity, StockAssumptions.carryingcapacity/StockAssumptions.mortalitycompensation), sum(StockDB.population[end,:][1,])))
   end
   @assert(0.01 < compensation_factor < 1.99, "Population regulation has failed, respecify simulation parameters")
-  for i = 1:(length(StockAssumptions.natural_mortality)-1)
-    stock_size[i+1] = rand(Binomial(StockDB.population[end,i], 1-(StockAssumptions.natural_mortality[i]*compensation_factor)))
+  for i = 1:(length(StockAssumptions.naturalmortality)-1)
+    stock_size[i+1] = rand(Binomial(StockDB.population[end,i], 1-(StockAssumptions.naturalmortality[i]*compensation_factor)))
   end
-  stock_size[end] += rand(Binomial(StockDB.population[end,end], 1-(StockAssumptions.natural_mortality[end]*compensation_factor)))
+  stock_size[end] += rand(Binomial(StockDB.population[end,end], 1-(StockAssumptions.naturalmortality[end]*compensation_factor)))
   push!(StockDB.population, DataArray(stock_size))
 end
