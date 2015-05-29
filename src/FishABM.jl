5a6d2b2423d915ef5fd3d34bae63eae96efa8b8e
Justin Angevaare <jangevaa@uoguelph.ca>
2015-05-29 11:11:18 -0400

    rename package...

---
+++ src/FishABM.jl
@@ -0,0 +1,40 @@
+module Fish_ABM
+
+using DataFrames, Distributions, Gadfly
+
+export
+  # Types
+  AgentAssumptions,
+  EnvironmentAssumptions,
+  StockAssumptions,
+  StockDB,
+
+  # Agent functions
+  AgentDB,
+  Kill!,
+  LocalMovement,
+  Move!,
+  InjectAgents!,
+
+  # Stock functions
+  Harvest!,
+  AgeAdults!,
+
+  # Agent-Stock function
+  Spawn!,
+  Graduate!,
+
+  # Utilities
+  MovementMatrix,
+
+  # Simulate
+  Simulate
+
+include("types.jl")
+include("utilities.jl")
+include("agents.jl")
+include("stock.jl")
+include("agent_stock_interaction.jl")
+include("simulate.jl")
+
+end # module
