30d525ea7cc94076cbc39b932a6d55b92ae0ab10
Justin Angevaare <jangevaa@uoguelph.ca>
2015-06-04 15:30:56 -0400

    change types...

--- src/types.jl
+++ src/types.jl
@@ -37,7 +37,6 @@ type StockAssumptions
   Age specific survivorship (survivorship at carrying capacity if density depedence occurs)
   Age at 50% mature (Binomial cdf assumed)
   Age specific fecundity (i.e. mean quantity of eggs each spawning female will produce)
-  Carrying capacity - overall carrying capacity
   Compensatory fecundity - compensatory strength for changes in fecundity. Compensatory strength is a divisor of K which will result in a 68% change in fecundity - smaller values indicate lower compensation strength. Compensation function based on Normal CDF. Use NaN if compensation is assumed to not occur.
   Compensatory sexual maturity - compensatory strength for changes in age of sexual maturity. Compensatory strength is a divisor of K which will result in a 68% change in age of sexual maturity - smaller values indicate lower compensation strength. Compensation function based on Normal CDF. Use NaN if compensation is assumed to not occur.
   Compensatory mortality - compensatory strength for changes in age of sexual maturity. Compensatory strength is a divisor of K which will result in a 68% change in natural mortality - smaller values indicate lower compensation strength. Compensation function based on Normal CDF. Use NaN if compensation is assumed to not occur.
@@ -46,7 +45,6 @@ Age specific catachability
   naturalmortality::Vector
   halfmature::Float64
   broodsize::Vector
-  carryingcapacity::Float64
   fecunditycompensation::Float64
   maturitycompensation::Float64
   mortalitycompensation::Float64
