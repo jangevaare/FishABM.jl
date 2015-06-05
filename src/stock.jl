4c895a97427716c836341c4d55a5720b22010970
Justin Angevaare <jangevaa@uoguelph.ca>
2015-06-05 10:28:52 -0400

    add stock size bump

--- src/stock.jl
+++ src/stock.jl
@@ -4,34 +4,35 @@ Justin Angevaare
 May 2015
 """
 
-function harvest!(effort::Float64, StockDB::StockDB, StockAssumptions::StockAssumptions)
+function harvest!(effort::Float64, stockdb::StockDB, stockassumptions::StockAssumptions)
   """
   This function will generate fishing mortality based on a specified integer level of effort, and assumed age specific catchabilities.
   """
-  harvest_size = fill(0, size(StockAssumptions.catchability))
+  harvest_size = fill(0, size(stockassumptions.catchability))
   if effort > 0
-    for i = 1:length(StockAssumptions.catchability)
-      harvest_size[i] = rand(Poisson(StockDB.population[end,i]*StockAssumptions.catchability[i]*effort))
-      StockDB.population[end, i] -= harvest_size[i]
+    for i = 1:length(stockassumptions.catchability)
+      harvest_size[i] = rand(Poisson(stockdb.population[end,i]*stockassumptions.catchability[i]*effort))
+      stockdb.population[end, i] -= harvest_size[i]
     end
   end
-  push!(StockDB.harvest, DataArray(harvest_size))
+  push!(stockdb.harvest, DataArray(harvest_size))
 end
 
-function ageadults!(StockDB::StockDB, StockAssumptions::StockAssumptions,carryingcapacity::Float64)
+function ageadults!(stockdb::StockDB, stockassumptions::StockAssumptions, carryingcapacity::Float64, bump=0::Int64)
   """
-  This function will apply transition probabilities to the current adult population. In the future this function may also apply annual removals due to fishing or other causes of mortality.
+  This function will apply transition probabilities to the current adult population.
   """
-  stock_size = fill(0, size(StockAssumptions.naturalmortality))
-  if isnan(StockAssumptions.mortalitycompensation)
+  stock_size = fill(0, size(stockassumptions.naturalmortality))
+  if isnan(stockassumptions.mortalitycompensation)
     compensation_factor = 1
   else
-    compensation_factor = 2*(cdf(Normal(carryingcapacity, carryingcapacity/StockAssumptions.mortalitycompensation), sum(StockDB.population[end,:][1,])))
+    compensation_factor = 2*(cdf(Normal(carryingcapacity, carryingcapacity/stockassumptions.mortalitycompensation), sum(stockdb.population[end,:][1,])))
   end
   @assert(0.01 < compensation_factor < 1.99, "Population regulation has failed, respecify simulation parameters")
-  for i = 1:(length(StockAssumptions.naturalmortality)-1)
-    stock_size[i+1] = rand(Binomial(StockDB.population[end,i], 1-(StockAssumptions.naturalmortality[i]*compensation_factor)))
+  for i = 1:(length(stockassumptions.naturalmortality)-1)
+    stock_size[i+1] = rand(Binomial(stockdb.population[end,i], 1-(stockassumptions.naturalmortality[i]*compensation_factor)))
   end
-  stock_size[end] += rand(Binomial(StockDB.population[end,end], 1-(StockAssumptions.naturalmortality[end]*compensation_factor)))
-  push!(StockDB.population, DataArray(stock_size))
+  stock_size[end] += rand(Binomial(stockdb.population[end,end], 1-(stockassumptions.naturalmortality[end]*compensation_factor)))
+  stock_size[1] += bump
+  push!(stockdb.population, DataArray(stock_size))
 end
