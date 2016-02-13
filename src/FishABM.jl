8793537badcca4e002775a20cbe9cf7468da90cf
devrows <drose02@mail.uoguelph.ca>
2016-02-13 13:27:56 -0500

    Fixed indentation of FishABM module

--- src/FishABM.jl
+++ src/FishABM.jl
@@ -1,49 +1,51 @@
+# module
+
 module FishABM
 
-using DataFrames, Distributions, Gadfly, ProgressMeter
+  using DataFrames, Distributions, Gadfly, ProgressMeter
 
-export
-  # Types
-  AgentAssumptions,
-  EnvironmentAssumptions,
-  StockAssumptions,
-  StockDB,
+  export
+    # Types
+    AgentAssumptions,
+    EnvironmentAssumptions,
+    StockAssumptions,
+    StockDB,
 
-  # agent_stock_interaction.jl functions
-  spawn!,
-  graduate!,
+    # agent_stock_interaction.jl functions
+    spawn!,
+    graduate!,
 
-  # agents.jl functions
-  AgentDB,
-  kill!,
-  localmove,
-  move!,
-  injectagents!,
+    # agents.jl functions
+    AgentDB,
+    kill!,
+    localmove,
+    move!,
+    injectagents!,
 
-  # simulationResults.jl functions
-  convertToStringArray,
-  simulationSummary,
+    # simulationResults.jl functions
+    convertToStringArray,
+    simulationSummary,
 
-  # stock.jl functions
-  harvest!,
-  ageadults!,
+    # stock.jl functions
+    harvest!,
+    ageadults!,
 
-  # utilities.jl functions
-  pad_environment!,
-  plot_agents,
-  plot_stock,
-  plot_stock_k,
-  writeOutAgentPlots,
+    # utilities.jl functions
+    pad_environment!,
+    plot_agents,
+    plot_stock,
+    plot_stock_k,
+    writeOutAgentPlots,
 
-  # simulate.jl functions
-  simulate
+    # simulate.jl functions
+    simulate
 
-include("types.jl")
-include("utilities.jl")
-include("agents.jl")
-include("stock.jl")
-include("agent_stock_interaction.jl")
-include("simulate.jl")
-include("simulationResults.jl")
+    include("types.jl")
+    include("utilities.jl")
+    include("agents.jl")
+    include("stock.jl")
+    include("agent_stock_interaction.jl")
+    include("simulate.jl")
+    include("simulationResults.jl")
 
-end # module
+end
